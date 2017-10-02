cebb881ebbcedcf25f2f8e294c3aef4126a33cbc
91085924 <91085924@mvusd.net>
2017-10-02 10:59:49 -0700

    HiLow4

---
+++ src/org/teachingkidsprogramming/section03ifs/HiLow4.java
@@ -0,0 +1,47 @@
+package org.teachingkidsprogramming.section03ifs;
+
+import java.util.Random;
+
+import org.teachingextensions.logo.Sound;
+import org.teachingextensions.logo.utils.EventUtils.MessageBox;
+
+public class HiLow4
+{
+  public static void main(String[] args)
+  {
+    Random rand = new Random();
+    int answer = rand.nextInt(100) + 1;
+    // int answer = (int) (Math.random() * 100 + 1);
+    //System.out.println("The answer is " + answer);
+    int upperRange = 100;
+    int NumberOfGuesses = MessageBox.askForNumericalInput("How many guesses would you like?");;
+    for (int i = 1; i <= NumberOfGuesses; i++)
+    {
+      int guess = MessageBox.askForNumericalInput("What is your guess?");
+      //input validation
+      while (guess < 1 || guess > upperRange)
+      {
+        MessageBox.showMessage("Please enter a valid guess 1 and" + upperRange);
+        guess = MessageBox.askForNumericalInput("What is your guess?");
+      }
+      if (guess == answer)
+      {
+        Sound.playBeep();
+        MessageBox.showMessage("You won!!");
+        break;
+      }
+      else if (guess > answer)
+      {
+        MessageBox.showMessage("Too high");
+      }
+      else
+      {
+        MessageBox.showMessage("Too low");
+      }
+      if (i == NumberOfGuesses)
+      {
+        MessageBox.showMessage("You lost");
+      }
+    }
+  }
+}
