e8f01091fa4411556599f29116121856fff08171
91085924 <91085924@mvusd.net>
2017-10-30 10:56:38 -0700

    Worked on pentagonCrazy

--- src/org/teachingkidsprogramming/section04mastery/PentagonCrazy.java
+++ src/org/teachingkidsprogramming/section04mastery/PentagonCrazy.java
@@ -10,43 +10,38 @@ public class PentagonCrazy
   {
     Tortoise.show();
     Tortoise.setSpeed(10);
-    //
     createColorPalette();
-    //
-    //    drawPentagon (recipe below) --#10.1
-    //
-    //    ------------- Recipe for drawPentagon --#10.2
+    drawPentagon();
+  }
+  private static void drawPentagon()
+  {
     for (int i = 0; i < 200; i++)
     {
-      //
       adjustPen();
-      //
-      //        The current length of a side is the same as the number of the side you are about to draw ( 1st side = 1 pixel, 2nd side = 2 pixels, etc) --#4.2
       int length = i;
-      //        Move the tortoise the length of a side --#4.1
       Tortoise.move(length);
       Tortoise.turn(360 / 5);
-      //        Turn the tortoise 1 more degree --#5
-      Tortoise.turn(+1);
+      Tortoise.turn(1);
     }
-    //    ------------- End of drawPentagon recipe --#10.3
   }
   private static void adjustPen()
   {
-    //        ------------- Recipe for adjustPen --#9.2
-    //        Change the pen color of the line the tortoise draws to the next color on the color wheel --#6
     Tortoise.setPenColor(ColorWheel.getNextColor());
-    //        Increase the tortoises pen width by 1 --#15                                              
+    //        Increase the tortoises pen width by 1 --#15
+    Tortoise.setPenWidth(+1);
     //        If the tortoise's pen width is greater than 4, then --#17
+    //if (penwidth > 4)
+    //{
+    //}
     //            Reset the pen width to 1 --#16
-    //        ------------- End of adjustPen recipe --#9.3 
+    Tortoise.setPenWidth(1);
   }
   private static void createColorPalette()
   {
     ColorWheel.addColor(PenColors.Blues.SteelBlue);
-    //    Add dark orchid to the color wheel --#11
-    //    Add dark slate blue to the color wheel --#12
-    //    Add teal to the color wheel --#13
-    //    Add indigo to the color wheel --#14
+    ColorWheel.addColor(PenColors.Purples.DarkOrchid);
+    ColorWheel.addColor(PenColors.Blues.DarkSlateBlue);
+    ColorWheel.addColor(PenColors.Greens.Teal);
+    ColorWheel.addColor(PenColors.Purples.Indigo);
   }
 }
