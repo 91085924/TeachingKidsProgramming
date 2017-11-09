11e12fc166bcc00896305fe75a0bcbe7636a213c
91085924 <91085924@mvusd.net>
2017-11-09 14:29:57 -0800

    created turtleTree

--- src/org/teachingkidsprogramming/section04mastery/BackgroundPhoto.java
+++ src/org/teachingkidsprogramming/section04mastery/BackgroundPhoto.java
@@ -10,23 +10,21 @@ public class BackgroundPhoto
   @SuppressWarnings("unused")
   public static void main(String[] args)
   {
-    //  Set the current picture to this url "https://usatftw.files.wordpress.com/2017/05/spongebob.jpg?w=1000&h=600&crop=1" --#1.0
     String currPicture = "https://usatftw.files.wordpress.com/2017/05/spongebob.jpg?w=1000&h=600&crop=1";
-    //  Set the background image to the current picture--#1.1
     Tortoise.getBackgroundWindow().setBackgroundImage(currPicture);
     Tortoise.show();
     Tortoise.setSpeed(10);
     int side = 2;
-    //  Do the following 75 times --#7.1
-    //     Set the current pen color to yellow --#6.1
-    Color currColor = PenColors.Yellows.Yellow;
-    Tortoise.setPenColor(currColor);
-    side++;
-    Tortoise.move(side);
-    Tortoise.setX(555);
-    Tortoise.setY(65);
-    Tortoise.turn(360 / 3);
-    Tortoise.turn(1);
-    //  End Repeat --#7.2
+    for (int i = 0; i < 75; i++)
+    {
+      Color currColor = PenColors.Yellows.Yellow;
+      Tortoise.setPenColor(currColor);
+      side++;
+      Tortoise.move(side);
+      Tortoise.setX(555);
+      Tortoise.setY(65);
+      Tortoise.turn(360 / 3);
+      Tortoise.turn(1);
+    }
   }
 }
